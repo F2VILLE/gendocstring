99991fad99f41ff2c12286959a84e935bb0ef2de
F2Ville <feliciendeville@gmail.com>
2024-03-14 21:23:00 +0100

    Fixing install.sh

--- install.sh
+++ install.sh
@@ -17,5 +17,4 @@ curl -s https://raw.githubusercontent.com/F2Ville/gendocstring/master/gendocstri
 chmod +x /usr/local/bin/gendocstring
 
 echo "gendocstring installed successfully !"
-echo .
 echo "(You may need to restart your terminal to use gendocstring.)"
