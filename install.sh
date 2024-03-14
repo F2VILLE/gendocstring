659a54767414a30d860202e35627ab4da43b0c31
F2Ville <feliciendeville@gmail.com>
2024-03-14 21:18:31 +0100

    Fixing install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
+if [ -f /usr/local/bin/gendocstring ]; then
+    echo "gendocstring is already installed. Exiting."
+    exit 1
+fi
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+echo "Installing gendocstring..."
+
 curl -s https://raw.githubusercontent.com/F2Ville/gendocstring/master/gendocstring.py > /usr/local/bin/gendocstring
 
 chmod +x /usr/local/bin/gendocstring
 
 echo "gendocstring installed successfully !"
 echo .
-echo "(You may need to restart your terminal to use gendocstring.)
+echo "(You may need to restart your terminal to use gendocstring.)"
